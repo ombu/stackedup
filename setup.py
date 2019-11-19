afdc7cdf37c0ff215e12b84b2b5c187284332582
Martin Rio <martin@ombuweb.com>
2019-11-19 15:14:08 -0800

    Updates the package version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stackedup",
-    version="0.0.1",
+    version="0.0.2",
     author="OMBU",
     author_email="martin@ombuweb.com",
     url="https://github.com/ombu/stacks",
