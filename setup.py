f18e9860436fecd5d34a6197f0eac4748488600d
Martin Rio <martin@ombuweb.com>
2024-03-01 12:37:04 -0800

    Tag release 0.0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stackedup",
-    version="0.0.12",
+    version="0.0.13",
     author="OMBU",
     author_email="martin@ombuweb.com",
     url="https://github.com/ombu/stacks",
